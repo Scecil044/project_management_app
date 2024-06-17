4e64c8c7f4963e27c539a958a939434b71779e8d
Scecil044 <scecil072@gmail.com>
2024-06-17 22:03:45 +0300

    last changes

--- server/src/index.js
+++ server/src/index.js
@@ -24,11 +24,10 @@ app.use(cookieParser());
 app.use("/api/v1/", appRoutes);
 
 // Deployment variables
-app.use(express.static(path.join(__dirname, "../../client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
-
 // app middleware
 app.use((err, req, res, next) => {
   const message = err.message || "Internal server error";
